03d5a01818e988351e86dc053ee9fe692a381a29
Softwareincorp <105108977+Softwareincorp@users.noreply.github.com>
2023-09-01 19:21:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var confetti = {
 	alpha: 1.0,		//the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
 	gradient: true,	//whether to use gradients for the confetti particles
 	start: null,		//call to start confetti animation (with optional timeout in milliseconds, and optional min and max random confetti count)
-	stop: false,		//call to stop adding confetti
+	stop: null,		//call to stop adding confetti
 	toggle: null,		//call to start or stop the confetti animation depending on whether it's already running
 	pause: null,		//call to freeze confetti animation
 	resume: null,		//call to unfreeze confetti animation
